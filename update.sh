782dcad949c58d11aa35dfb3485a0a7cc4473711
Enoch Yu <247741827+enochyu-official@users.noreply.github.com>
2026-08-21 14:31:54 +0900

    add update script

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cp LibreNotebook.pdf \
+  ~/websites/archive.enochyu.com/static/mathematics/pdf/26100.pdf
+
+cd ~/websites/archive.enochyu.com
+
+tmp=$(mktemp)
+jq ".[-1].date = \"$(date '+%d %B %Y')\"" data/math04.json > "$tmp" \
+  && mv "$tmp" data/math04.json
+
+tmp=$(mktemp)
+jq ".[-1].date = \"$(date '+%d %B %Y')\"" data/featured.json > "$tmp" \
+  && mv "$tmp" data/featured.json
+
+tmp=$(mktemp)
+jq ".[0].date = \"$(date '+%d %B %Y') - Last Updated\"" \
+  data/disciplines.json > "$tmp" && mv "$tmp" data/disciplines.json
+
+rm -r public
+hugo
+
+git add .
+git commit -S -m "Update LibreNotebook"
+git push origin main
+
